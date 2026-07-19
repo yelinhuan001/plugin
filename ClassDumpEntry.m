4d589163ab568feadf0bac5a495ae0a89ede71d7
yelinhuan001 <61222411+yelinhuan001@users.noreply.github.com>
2026-07-19 23:38:08 +0800

    fix: ClassDumpEntry.m

--- ClassDumpEntry.m
+++ ClassDumpEntry.m
@@ -1,21 +1,3 @@
-#import <UIKit/UIKit.h>
-
-// 前向声明
-@interface FloatingButton : UIView
-+ (void)show;
-+ (void)hide;
-@end
-
-@interface ToolboxViewController : UIViewController
-+ (void)show;
-+ (void)dismiss;
-+ (BOOL)isVisible;
-@end
-
-static void __attribute__((constructor)) initialize() {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        // 在 keyWindow 上添加浮动按钮
-        [FloatingButton show];
-        NSLog(@"[Hacker] 插件已注入。点击 🔧 浮动按钮唤出工具箱。");
-    });
-}
+// 入口已迁移到 ToolboxViewController.m (__attribute__((constructor)))
+// 此文件保留以确保 Makefile 引用不报错
+// 所有初始化逻辑在 ToolboxViewController.m 的 toolbox_init() 中
