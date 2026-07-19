5c9dd76d59847c100a5327c7a378178523113b24
yelinhuan001 <61222411+yelinhuan001@users.noreply.github.com>
2026-07-19 22:36:56 +0800

    fix: 修复 SearchOverlayWindow.m — 编译错误/缺失实现/废弃API

--- SearchOverlayWindow.m
+++ SearchOverlayWindow.m
@@ -9,7 +9,8 @@
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         instance = [[self alloc] initWithFrame:[UIScreen mainScreen].bounds];
-        instance.windowLevel = UIWindowLevelStatusBar - 2;
+        // 修复：UIWindowLevelStatusBar 在 iOS 13+ 已废弃，使用数值常量
+        instance.windowLevel = 2098.0; // 原 UIWindowLevelStatusBar - 2
         instance.rootViewController = [UIViewController new];
         instance.hidden = YES;
     });
@@ -18,10 +19,15 @@
 
 - (void)showTip:(NSString *)title msg:(NSString *)msg {
     dispatch_async(dispatch_get_main_queue(), ^{
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:title message:msg preferredStyle:UIAlertControllerStyleAlert];
-        [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil]];
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:title
+                                                                       message:msg
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"确定"
+                                                  style:UIAlertActionStyleDefault
+                                                handler:nil]];
         [self makeKeyAndVisible];
         [self.rootViewController presentViewController:alert animated:YES completion:nil];
     });
 }
+
 @end
